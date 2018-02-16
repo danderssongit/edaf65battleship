fa684d7862ffeff0edc4304b79cc71448c611efb
danderssongit <dandersson23@gmail.com>
2018-02-16 10:55:05 +0100

    first commit

---
+++ app/src/main/java/se/lth/soc13dan/battleshipsedaf65/MainMenu.java
@@ -0,0 +1,13 @@
+package se.lth.soc13dan.battleshipsedaf65;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
---
+++ app/src/main/java/se/lth/soc13dan/battleshipsedaf65/Player.java
@@ -0,0 +1,8 @@
+package se.lth.soc13dan.battleshipsedaf65;
+
+/**
+ * Created by soc13dan on 2018-02-16.
+ */
+
+public class Player {
+}
